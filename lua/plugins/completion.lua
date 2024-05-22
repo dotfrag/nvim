db50785769d082abf158383f2c5dc64f7e195e47
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-05-22 13:50:26 +0300

    update completion keymaps

---
+++ lua/plugins/completion.lua
@@ -0,0 +1,32 @@
+return {
+  "nvim-cmp",
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    opts.mapping["<CR>"] = nil
+    opts.mapping["<C-y>"] = LazyVim.cmp.confirm()
+    opts.mapping["<C-d>"] = cmp.mapping.scroll_docs(4)
+    opts.mapping["<C-u>"] = cmp.mapping.scroll_docs(-4)
+  end,
+  keys = function()
+    return {
+      {
+        "<C-l>",
+        function()
+          return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = { "i", "s" },
+      },
+      {
+        "<C-h>",
+        function()
+          return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<Tab>"
+        end,
+        expr = true,
+        silent = true,
+        mode = { "i", "s" },
+      },
+    }
+  end,
+}
