4e185ebc36e6c53ccfcd2410cdd9bc1f049a9ce4
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-05-22 19:23:00 +0300

    add disable_autoformat autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,12 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
+local function augroup(name)
+  return vim.api.nvim_create_augroup("dotvim_" .. name, { clear = true })
+end
+
+-- disable autoformat for yazi config files
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = augroup("disable_autoformat"),
+  pattern = "*/yazi/*.toml",
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
