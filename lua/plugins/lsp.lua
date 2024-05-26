1a5786e1e67c8d13880873ea4e9bf37cdbaa250c
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-05-26 13:28:48 +0300

    do not use biome for astro

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,16 @@
+local biome = require("lspconfig.server_configurations.biome")
+local biome_ft = biome.default_config.filetypes
+biome_ft = vim.tbl_filter(function(ft)
+  return not vim.tbl_contains({ "astro" }, ft)
+end, biome_ft)
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
       bashls = {},
       astro = {},
-      biome = {},
+      biome = { filetypes = biome_ft },
     },
   },
 }
