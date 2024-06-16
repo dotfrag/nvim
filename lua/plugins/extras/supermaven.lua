b04fb5ae53326071673f87c549ac9e3048c7f72b
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-06-16 11:15:20 +0300

    update supermaven

--- lua/plugins/extras/supermaven.lua
+++ lua/plugins/extras/supermaven.lua
@@ -1,18 +1,35 @@
 return {
-  "nvim-cmp",
-  dependencies = {
-    "supermaven-inc/supermaven-nvim",
-    build = ":SupermavenUseFree",
-    opts = {
-      disable_inline_completion = true,
+  {
+    "nvim-cmp",
+    dependencies = {
+      "supermaven-inc/supermaven-nvim",
+      build = ":SupermavenUseFree",
+      opts = {
+        disable_inline_completion = true,
+      },
     },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, 1, {
+        name = "supermaven",
+        group_index = 1,
+        priority = 100,
+      })
+    end,
   },
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    table.insert(opts.sources, 1, {
-      name = "supermaven",
-      group_index = 1,
-      priority = 100,
-    })
-  end,
+
+  {
+    "folke/noice.nvim",
+    opts = function(_, opts)
+      vim.list_extend(opts.routes, {
+        {
+          filter = {
+            event = "msg_show",
+            find = "Supermaven Free Tier",
+          },
+          skip = true,
+        },
+      })
+    end,
+  },
 }
