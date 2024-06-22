ace55733694ddb3bf61d9cc7fa09824827d64af9
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-06-22 16:11:56 +0300

    update fzf keymaps

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,12 +1,23 @@
+local rg_opts = ""
+if LazyVim.has_extra("editor.fzf") then
+  rg_opts = require("fzf-lua").defaults.grep.rg_opts
+end
+
 return {
   "ibhagwan/fzf-lua",
   optional = true,
-  -- stylua: ignore
   keys = {
     { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "Search Buffer" },
     { "<leader>sf", LazyVim.pick("auto"), desc = "Find Files (Root Dir)" },
     { "<leader>s.", "<cmd>FzfLua resume<cr>", desc = "Resume" },
     { "<leader>fl", LazyVim.pick("files", { cwd = LazyVim.get_plugin_path("LazyVim") }), desc = "Find Plugin Files" },
-    { "<leader>fL", LazyVim.pick("live_grep", { cwd = LazyVim.get_plugin_path("LazyVim") }), desc = "Grep Plugin Files" },
+    {
+      "<leader>fL",
+      LazyVim.pick("live_grep", {
+        cwd = LazyVim.get_plugin_path("LazyVim"),
+        rg_opts = "-g '*.lua' " .. rg_opts,
+      }),
+      desc = "Grep Plugin Files",
+    },
   },
 }
