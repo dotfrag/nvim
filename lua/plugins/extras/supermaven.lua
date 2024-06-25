df4ee5dd8e9b2a024a7625b7b2fafb077e8aa256
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-06-25 11:07:29 +0300

    update supermaven

--- lua/plugins/extras/supermaven.lua
+++ lua/plugins/extras/supermaven.lua
@@ -1,22 +1,29 @@
 return {
   {
-    "nvim-cmp",
-    dependencies = {
-      "supermaven-inc/supermaven-nvim",
-      build = ":SupermavenUseFree",
-      opts = {
-        disable_inline_completion = true,
-      },
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      table.insert(opts.sources, 1, {
-        name = "supermaven",
-        group_index = 1,
-        priority = 100,
-      })
-    end,
+    "supermaven-inc/supermaven-nvim",
+    event = "LazyFile",
+    build = ":SupermavenUseFree",
+    opts = {},
   },
+
+  -- {
+  --   "nvim-cmp",
+  --   dependencies = {
+  --     "supermaven-inc/supermaven-nvim",
+  --     build = ":SupermavenUseFree",
+  --     opts = {
+  --       disable_inline_completion = true,
+  --     },
+  --   },
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     table.insert(opts.sources, 1, {
+  --       name = "supermaven",
+  --       group_index = 1,
+  --       priority = 100,
+  --     })
+  --   end,
+  -- },
 
   {
     "folke/noice.nvim",
