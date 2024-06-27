c986c28525e13219e1f2d6ac38a2f1a1c96b08f7
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-06-27 20:50:19 +0300

    refactor lsp config
    https://github.com/LazyVim/LazyVim/discussions/3829

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,12 @@
-local biome = require("lspconfig.server_configurations.biome")
-local biome_ft = biome.default_config.filetypes
-biome_ft = vim.tbl_filter(function(ft)
-  return not vim.tbl_contains({ "astro" }, ft)
-end, biome_ft)
-
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      biome = { filetypes = biome_ft },
-    },
-  },
+  opts = function(_, opts)
+    local biome = require("lspconfig.server_configurations.biome")
+    local biome_ft = biome.default_config.filetypes
+    biome_ft = vim.tbl_filter(function(ft)
+      return not vim.tbl_contains({ "astro" }, ft)
+    end, biome_ft)
+
+    opts.servers.biome = { filetypes = biome_ft }
+  end,
 }
