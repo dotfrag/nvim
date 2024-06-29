2ceb37b440adb84fba8a437a35b194d7d3db8836
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-06-29 14:02:14 +0300

    refactor fzf config

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,30 +1,28 @@
-local fd_opts = ""
-local rg_opts = ""
-if LazyVim.has_extra("editor.fzf") then
-  fd_opts = require("fzf-lua").defaults.files.fd_opts
-  rg_opts = require("fzf-lua").defaults.grep.rg_opts
-end
-
 return {
   "ibhagwan/fzf-lua",
   optional = true,
-  keys = {
-    { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "Search Buffer" },
-    { "<leader>sf", LazyVim.pick("auto"), desc = "Find Files (Root Dir)" },
-    { "<leader>s.", "<cmd>FzfLua resume<cr>", desc = "Resume" },
-    {
-      "<leader>fd",
-      LazyVim.pick("files", { fd_opts = ". ~/repos/dotfiles ~/repos/dotfiles-private " .. fd_opts }),
-      desc = "Find Dotfiles",
-    },
-    { "<leader>fl", LazyVim.pick("files", { cwd = LazyVim.get_plugin_path("LazyVim") }), desc = "Find Plugin Files" },
-    {
-      "<leader>fL",
-      LazyVim.pick("live_grep", {
-        cwd = LazyVim.get_plugin_path("LazyVim"),
-        rg_opts = "-g '*.lua' " .. rg_opts,
-      }),
-      desc = "Grep Plugin Files",
-    },
-  },
+  keys = function(_, keys)
+    local fd_opts = require("fzf-lua").defaults.files.fd_opts
+    local rg_opts = require("fzf-lua").defaults.grep.rg_opts
+
+    vim.list_extend(keys, {
+      { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "Search Buffer" },
+      { "<leader>sf", LazyVim.pick("auto"), desc = "Find Files (Root Dir)" },
+      { "<leader>s.", "<cmd>FzfLua resume<cr>", desc = "Resume" },
+      {
+        "<leader>fd",
+        LazyVim.pick("files", { fd_opts = ". ~/repos/dotfiles ~/repos/dotfiles-private " .. fd_opts }),
+        desc = "Find Dotfiles",
+      },
+      { "<leader>fl", LazyVim.pick("files", { cwd = LazyVim.get_plugin_path("LazyVim") }), desc = "Find Plugin Files" },
+      {
+        "<leader>fL",
+        LazyVim.pick("live_grep", {
+          cwd = LazyVim.get_plugin_path("LazyVim"),
+          rg_opts = "-g '*.lua' " .. rg_opts,
+        }),
+        desc = "Grep Plugin Files",
+      },
+    })
+  end,
 }
