37159ae5090a237df8881ff7de646aa4189373bd
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-07-07 13:06:08 +0300

    add dashboard autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -14,6 +14,7 @@ autocmd("BufEnter", {
 })
 
 -- close some filetypes with <q>
+-- https://github.com/LazyVim/LazyVim/blob/6202dd164466250b5c188918b34b3e8a3fec2604/lua/lazyvim/config/autocmds.lua#L53
 autocmd("FileType", {
   group = augroup("close_with_q"),
   pattern = {
@@ -22,5 +23,20 @@ autocmd("FileType", {
   callback = function(event)
     vim.bo[event.buf].buflisted = false
     vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+  end,
+})
+
+-- open dashboard when closing all buffers
+-- https://github.com/LazyVim/LazyVim/discussions/3901#discussioncomment-9976798
+vim.api.nvim_create_autocmd("BufDelete", {
+  group = augroup("dashboard_on_empty"),
+  callback = function(args)
+    local deleted_name = vim.api.nvim_buf_get_name(args.buf)
+    local deleted_ft = vim.api.nvim_get_option_value("filetype", { buf = args.buf })
+    local dashboard_on_empty = (deleted_name == "" and deleted_ft == "")
+      or (vim.api.nvim_buf_get_name(0) == "" and vim.api.nvim_get_option_value("filetype", { buf = 0 }) == "")
+    if dashboard_on_empty then
+      vim.cmd("Dashboard")
+    end
   end,
 })
