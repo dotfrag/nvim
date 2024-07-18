bc8235820e60d31635f653e9694e6f9a8dd8d3ec
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-07-18 16:15:45 +0300

    update autocmds

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -4,15 +4,6 @@ end
 
 local autocmd = vim.api.nvim_create_autocmd
 
--- disable autoformat for yazi config files
-autocmd("BufEnter", {
-  group = augroup("disable_autoformat"),
-  pattern = "*/yazi/*.toml",
-  callback = function()
-    vim.b.autoformat = false
-  end,
-})
-
 -- close some filetypes with <q>
 -- https://github.com/LazyVim/LazyVim/blob/6202dd164466250b5c188918b34b3e8a3fec2604/lua/lazyvim/config/autocmds.lua#L53
 autocmd("FileType", {
@@ -22,13 +13,13 @@ autocmd("FileType", {
   },
   callback = function(event)
     vim.bo[event.buf].buflisted = false
-    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true })
+    vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = event.buf, silent = true, desc = "Quit buffer" })
   end,
 })
 
 -- open dashboard when closing all buffers
 -- https://github.com/LazyVim/LazyVim/discussions/3901#discussioncomment-9976798
-vim.api.nvim_create_autocmd("BufDelete", {
+autocmd("BufDelete", {
   group = augroup("dashboard_on_empty"),
   callback = function(args)
     local deleted_name = vim.api.nvim_buf_get_name(args.buf)
@@ -40,3 +31,24 @@ vim.api.nvim_create_autocmd("BufDelete", {
     end
   end,
 })
+
+-- disable autoformat for yazi config files
+autocmd("BufEnter", {
+  group = augroup("disable_autoformat"),
+  pattern = "*/yazi/*.toml",
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
+
+-- -- organise imports for typescript files
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+--   group = augroup("organise_imports"),
+--   pattern = "*.ts",
+--   callback = function()
+--     ---@diagnostic disable-next-line: assign-type-mismatch
+--     vim.lsp.buf.code_action({ apply = true, context = { only = { "source.addMissingImports.ts" }, diagnostics = {} } })
+--     ---@diagnostic disable-next-line: assign-type-mismatch
+--     vim.lsp.buf.code_action({ apply = true, context = { only = { "source.removeUnused.ts" }, diagnostics = {} } })
+--   end,
+-- })
