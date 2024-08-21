a037aefdbc1a9cdec71df6e54fbb49e1c340d7f8
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-08-21 11:19:01 +0300

    add comment-box plugin

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -1,28 +1,35 @@
 return {
-  "stevearc/conform.nvim",
-  optional = true,
-  opts = {
-    formatters = {
-      shfmt = { prepend_args = { "-i", "2", "-ci" } },
-      biome = { prepend_args = { "format", "--indent-style", "space" } },
-    },
-    formatters_by_ft = {
-      fish = {}, -- reset lazyvim value
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters = {
+        shfmt = { prepend_args = { "-i", "2", "-ci" } },
+        biome = { prepend_args = { "format", "--indent-style", "space" } },
+      },
+      formatters_by_ft = {
+        fish = {}, -- reset lazyvim value
 
-      -- biome
-      javascript = { "biome" },
-      typescript = { "biome" },
-      json = { "biome" },
-      jsonc = { "biome" },
+        -- biome
+        javascript = { "biome" },
+        typescript = { "biome" },
+        json = { "biome" },
+        jsonc = { "biome" },
 
-      -- prettier
-      html = { "prettierd" },
-      css = { "prettierd" },
-      scss = { "prettierd" },
-      less = { "prettierd" },
-      yaml = { "prettierd" },
-      markdown = { "prettierd" },
-      ["markdown.mdx"] = { "prettierd" },
+        -- prettier
+        html = { "prettierd" },
+        css = { "prettierd" },
+        scss = { "prettierd" },
+        less = { "prettierd" },
+        yaml = { "prettierd" },
+        markdown = { "prettierd" },
+        ["markdown.mdx"] = { "prettierd" },
+      },
     },
+  },
+
+  {
+    "LudoPinelli/comment-box.nvim",
+    event = "LazyFile",
   },
 }
