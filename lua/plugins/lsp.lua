e77ea05414dbfc61944bde5203c3db3294cd1bc6
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-10-02 08:56:20 +0300

    rename lspconfig 'server_configurations' -> 'configs'
    https://github.com/neovim/nvim-lspconfig/commit/bedb2a0df105f68a624a49b867f269b6d55a2c89

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
   "neovim/nvim-lspconfig",
   opts = function(_, opts)
-    local biome = require("lspconfig.server_configurations.biome")
+    local biome = require("lspconfig.configs.biome")
     local biome_ft = biome.default_config.filetypes
     biome_ft = vim.tbl_filter(function(ft)
       return not vim.tbl_contains({ "astro" }, ft)
