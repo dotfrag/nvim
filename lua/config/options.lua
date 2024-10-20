6279b3f70812e472707472535da553e4d11b4408
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-10-20 13:59:36 +0300

    workaround for osc52

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -9,6 +9,25 @@ opt.diffopt:append("iwhite")
 -- Lines of context
 opt.scrolloff = 8
 
+-- Clipboard
+if vim.env.SSH_TTY then
+  opt.clipboard:append("unnamedplus")
+  local function paste()
+    return vim.split(vim.fn.getreg(""), "\n")
+  end
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end
+
 -- Neovide options
 if vim.g.neovide then
   vim.g.neovide_hide_mouse_when_typing = true
