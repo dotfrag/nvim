147f69508def930cccc75e23738538a463abc6ea
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-10-24 14:50:03 +0300

    move project picker to fzf util

--- lua/dotvim/util/fzf.lua
+++ lua/dotvim/util/fzf.lua
@@ -31,4 +31,86 @@ function M.plugin_files()
   fzf_lua.fzf_exec(plugins, opts)
 end
 
+function M.projects()
+  if LazyVim.pick.picker.name == "telescope" then
+    return vim.cmd("Telescope projects")
+  elseif LazyVim.pick.picker.name == "fzf" then
+    local project = require("project_nvim.project")
+    local history = require("project_nvim.utils.history")
+    local results = history.get_recent_projects()
+    local utils = require("fzf-lua.utils")
+
+    local function hl_validate(hl)
+      return not utils.is_hl_cleared(hl) and hl or nil
+    end
+
+    local function ansi_from_hl(hl, s)
+      return utils.ansi_from_hl(hl_validate(hl), s)
+    end
+
+    ------@param selected string[]
+    ------@param pick? boolean
+    ---local function cd(selected, pick)
+    ---  local path = selected[1]
+    ---  if vim.fn.getcwd() == vim.env.HOME then
+    ---    local ok = project.set_pwd(path)
+    ---    if ok and not pick then
+    ---      vim.api.nvim_win_close(0, false)
+    ---      LazyVim.info("Change project dir to " .. path)
+    ---    end
+    ---  end
+    ---  if pick then
+    ---    fzf_lua.files({ cwd = path })
+    ---  else
+    ---  end
+    ---end
+
+    local opts = {
+      fzf_opts = {
+        ["--header"] = string.format(
+          ":: <%s> to %s | <%s> to %s | <%s> to %s",
+          ansi_from_hl("FzfLuaHeaderBind", "ctrl-s"),
+          ansi_from_hl("FzfLuaHeaderText", "live_grep"),
+          ansi_from_hl("FzfLuaHeaderBind", "ctrl-r"),
+          ansi_from_hl("FzfLuaHeaderText", "oldfiles"),
+          ansi_from_hl("FzfLuaHeaderBind", "ctrl-w"),
+          ansi_from_hl("FzfLuaHeaderText", "change_dir")
+        ),
+      },
+      fzf_colors = true,
+      actions = {
+        ["default"] = {
+          function(selected)
+            -- cd(selected, true)
+            fzf_lua.files({ cwd = selected[1] })
+          end,
+        },
+        ["ctrl-s"] = {
+          function(selected)
+            fzf_lua.live_grep({ cwd = selected[1] })
+          end,
+        },
+        ["ctrl-r"] = {
+          function(selected)
+            fzf_lua.oldfiles({ cwd = selected[1] })
+          end,
+        },
+        ["ctrl-w"] = {
+          function(selected)
+            -- cd(selected)
+            local path = selected[1]
+            local ok = project.set_pwd(path)
+            if ok then
+              vim.api.nvim_win_close(0, false)
+              LazyVim.info("Change project dir to " .. path)
+            end
+          end,
+        },
+      },
+    }
+
+    fzf_lua.fzf_exec(results, opts)
+  end
+end
+
 return M
--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,92 +1,9 @@
-local pick = function()
-  if LazyVim.pick.picker.name == "telescope" then
-    return vim.cmd("Telescope projects")
-  elseif LazyVim.pick.picker.name == "fzf" then
-    local fzf_lua = require("fzf-lua")
-    local project = require("project_nvim.project")
-    local history = require("project_nvim.utils.history")
-    local results = history.get_recent_projects()
-    local utils = require("fzf-lua.utils")
-
-    local function hl_validate(hl)
-      return not utils.is_hl_cleared(hl) and hl or nil
-    end
-
-    local function ansi_from_hl(hl, s)
-      return utils.ansi_from_hl(hl_validate(hl), s)
-    end
-
-    ------@param selected string[]
-    ------@param pick? boolean
-    ---local function cd(selected, pick)
-    ---  local path = selected[1]
-    ---  if vim.fn.getcwd() == vim.env.HOME then
-    ---    local ok = project.set_pwd(path)
-    ---    if ok and not pick then
-    ---      vim.api.nvim_win_close(0, false)
-    ---      LazyVim.info("Change project dir to " .. path)
-    ---    end
-    ---  end
-    ---  if pick then
-    ---    fzf_lua.files({ cwd = path })
-    ---  else
-    ---  end
-    ---end
-
-    local opts = {
-      fzf_opts = {
-        ["--header"] = string.format(
-          ":: <%s> to %s | <%s> to %s | <%s> to %s",
-          ansi_from_hl("FzfLuaHeaderBind", "ctrl-s"),
-          ansi_from_hl("FzfLuaHeaderText", "live_grep"),
-          ansi_from_hl("FzfLuaHeaderBind", "ctrl-r"),
-          ansi_from_hl("FzfLuaHeaderText", "oldfiles"),
-          ansi_from_hl("FzfLuaHeaderBind", "ctrl-w"),
-          ansi_from_hl("FzfLuaHeaderText", "change_dir")
-        ),
-      },
-      fzf_colors = true,
-      actions = {
-        ["default"] = {
-          function(selected)
-            -- cd(selected, true)
-            fzf_lua.files({ cwd = selected[1] })
-          end,
-        },
-        ["ctrl-s"] = {
-          function(selected)
-            fzf_lua.live_grep({ cwd = selected[1] })
-          end,
-        },
-        ["ctrl-r"] = {
-          function(selected)
-            fzf_lua.oldfiles({ cwd = selected[1] })
-          end,
-        },
-        ["ctrl-w"] = {
-          function(selected)
-            -- cd(selected)
-            local path = selected[1]
-            local ok = project.set_pwd(path)
-            if ok then
-              vim.api.nvim_win_close(0, false)
-              LazyVim.info("Change project dir to " .. path)
-            end
-          end,
-        },
-      },
-    }
-
-    fzf_lua.fzf_exec(results, opts)
-  end
-end
-
 return {
   {
     "nvim-telescope/telescope.nvim",
     optional = true,
     keys = {
-      { "<leader>fp", pick, desc = "Projects" },
+      { "<leader>fp", DotVim.fzf.projects, desc = "Projects" },
     },
   },
 
@@ -94,7 +11,7 @@ return {
     "ibhagwan/fzf-lua",
     optional = true,
     keys = {
-      { "<leader>fp", pick, desc = "Projects" },
+      { "<leader>fp", DotVim.fzf.projects, desc = "Projects" },
     },
   },
 
@@ -102,7 +19,7 @@ return {
     "nvimdev/dashboard-nvim",
     optional = true,
     opts = function(_, opts)
-      local projects = { action = pick, desc = "Projects", icon = "", key = "p" }
+      local projects = { action = DotVim.fzf.projects, desc = "Projects", icon = "", key = "p" }
       table.insert(opts.config.center, 3, projects)
     end,
   },
