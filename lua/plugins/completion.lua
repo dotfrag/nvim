71273d2d39ab1aa5f03decabf55942b0b1f96ffd
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-11-04 09:17:27 +0200

    update completion config

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,65 +1,78 @@
-return {
-  "iguanacucumber/magazine.nvim",
-  name = "nvim-cmp",
-  optional = true,
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    opts.mapping["<CR>"] = function(fallback)
-      cmp.abort()
-      fallback()
-    end
-    opts.mapping["<C-y>"] = LazyVim.cmp.confirm()
-    opts.mapping["<C-d>"] = cmp.mapping.scroll_docs(4)
-    opts.mapping["<C-u>"] = cmp.mapping.scroll_docs(-4)
+local border_cmp = {
+  { "󱐋", "WarningMsg" },
+  { "─", "Comment" },
+  { "╮", "Comment" },
+  { "│", "Comment" },
+  { "╯", "Comment" },
+  { "─", "Comment" },
+  { "╰", "Comment" },
+  { "│", "Comment" },
+}
 
-    -- https://github.com/LazyVim/LazyVim/pull/4052#issuecomment-2249293126
-    opts.window = {
-      completion = cmp.config.window.bordered({
-        border = {
-          { "󱐋", "WarningMsg" },
-          { "─", "Comment" },
-          { "╮", "Comment" },
-          { "│", "Comment" },
-          { "╯", "Comment" },
-          { "─", "Comment" },
-          { "╰", "Comment" },
-          { "│", "Comment" },
+local border_doc = {
+  { "", "DiagnosticHint" },
+  { "─", "Comment" },
+  { "╮", "Comment" },
+  { "│", "Comment" },
+  { "╯", "Comment" },
+  { "─", "Comment" },
+  { "╰", "Comment" },
+  { "│", "Comment" },
+}
+
+return {
+  {
+    "iguanacucumber/magazine.nvim",
+    name = "nvim-cmp",
+    optional = true,
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping["<CR>"] = function(fallback)
+        cmp.abort()
+        fallback()
+      end
+      opts.mapping["<C-y>"] = LazyVim.cmp.confirm()
+      opts.mapping["<C-d>"] = cmp.mapping.scroll_docs(4)
+      opts.mapping["<C-u>"] = cmp.mapping.scroll_docs(-4)
+
+      -- https://github.com/LazyVim/LazyVim/pull/4052#issuecomment-2249293126
+      opts.window = {
+        completion = cmp.config.window.bordered({ border = border_cmp }),
+        documentation = cmp.config.window.bordered({ border = border_doc }),
+      }
+    end,
+    keys = function()
+      return {
+        {
+          "<C-l>",
+          function()
+            return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
+          end,
+          expr = true,
+          silent = true,
+          mode = { "i", "s" },
         },
-      }),
-      documentation = cmp.config.window.bordered({
-        border = {
-          { "", "DiagnosticHint" },
-          { "─", "Comment" },
-          { "╮", "Comment" },
-          { "│", "Comment" },
-          { "╯", "Comment" },
-          { "─", "Comment" },
-          { "╰", "Comment" },
-          { "│", "Comment" },
+        {
+          "<C-h>",
+          function()
+            return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<Tab>"
+          end,
+          expr = true,
+          silent = true,
+          mode = { "i", "s" },
         },
-      }),
-    }
-  end,
-  keys = function()
-    return {
-      {
-        "<C-l>",
-        function()
-          return vim.snippet.active({ direction = 1 }) and "<cmd>lua vim.snippet.jump(1)<cr>" or "<Tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = { "i", "s" },
+      }
+    end,
+  },
+
+  {
+    "saghen/blink.cmp",
+    optional = true,
+    opts = {
+      windows = {
+        autocomplete = { border = border_cmp },
+        documentation = { border = border_doc },
       },
-      {
-        "<C-h>",
-        function()
-          return vim.snippet.active({ direction = -1 }) and "<cmd>lua vim.snippet.jump(-1)<cr>" or "<Tab>"
-        end,
-        expr = true,
-        silent = true,
-        mode = { "i", "s" },
-      },
-    }
-  end,
+    },
+  },
 }
