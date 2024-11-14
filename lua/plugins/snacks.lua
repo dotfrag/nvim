89577b4da7ab51301647eb63e01a881c7e05ef66
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-11-14 09:45:33 +0200

    add snacks config

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  opts = {
+    words = {
+      debounce = 250, -- time in ms to wait before updating
+    },
+    styles = {
+      notification = {
+        wo = { wrap = true }, -- wrap notifications
+      },
+    },
+  },
+}
