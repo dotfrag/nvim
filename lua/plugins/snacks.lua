d097136f93737d2196585eb90eed03745f5fa1b6
dotfrag <17456867+dotfrag@users.noreply.github.com>
2024-12-11 16:43:32 +0200

    use snacks.indent instead of other indent plugins

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,13 +1,18 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    words = {
-      debounce = 250, -- time in ms to wait before updating
-    },
-    styles = {
-      notification = {
-        wo = { wrap = true }, -- wrap notifications
+  {
+    "folke/snacks.nvim",
+    opts = {
+      words = {
+        debounce = 250, -- time in ms to wait before updating
       },
+      styles = {
+        notification = {
+          wo = { wrap = true }, -- wrap notifications
+        },
+      },
+      indent = {},
     },
   },
+
+  { "lukas-reineke/indent-blankline.nvim", enabled = false },
 }
