7a9373ad5d3505768a54b2207cc2acd2628fc79a
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-02-09 15:00:22 +0200

    update snacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,8 +1,17 @@
 return {
   {
     "folke/snacks.nvim",
+    optional = true,
+    -- stylua: ignore
     keys = {
       { "<leader>dps", false },
+      { "<leader>/", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
+      { "<leader>sf", function() Snacks.picker.files() end, desc = "Find Files (Root Dir)" },
+      { "<leader>.", function() Snacks.picker.resume() end, desc = "Resume" },
+      { "<leader>fd", function() Snacks.picker.files({ dirs = { "~/repos/dotfiles", "~/repos/dotfiles-private" } }) end, desc = "Find Dotfiles" },
+      { "<leader>fl", function() Snacks.picker.files({ dirs = { LazyVim.get_plugin_path("LazyVim") } }) end, desc = "Find LazyVim Files" },
+      { "<leader>fL", function() Snacks.picker.grep({ dirs = { LazyVim.get_plugin_path("LazyVim") } }) end, desc = "Grep LazyVim Files" },
+      -- { "<leader>fP", DotVim.snacks.plugin_files, desc = "Find Plugin Files" },
     },
     opts = {
       words = {
