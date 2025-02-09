6100c99ddf18a0320a44b235cb5a5966bf18f9fa
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-02-09 15:19:30 +0200

    fix projects picker

--- lua/plugins/project.lua
+++ lua/plugins/project.lua
@@ -1,35 +1,35 @@
 return {
-  {
-    "nvim-telescope/telescope.nvim",
-    optional = true,
-    keys = {
-      { "<leader>fp", DotVim.fzf.projects, desc = "Projects" },
-    },
-  },
+  -- {
+  --   "nvim-telescope/telescope.nvim",
+  --   optional = true,
+  --   keys = {
+  --     { "<leader>fp", DotVim.fzf.projects, desc = "Projects" },
+  --   },
+  -- },
 
-  {
-    "ibhagwan/fzf-lua",
-    optional = true,
-    keys = {
-      { "<leader>fp", DotVim.fzf.projects, desc = "Projects" },
-    },
-  },
+  -- {
+  --   "ibhagwan/fzf-lua",
+  --   optional = true,
+  --   keys = {
+  --     { "<leader>fp", DotVim.fzf.projects, desc = "Projects" },
+  --   },
+  -- },
 
-  {
-    "nvimdev/dashboard-nvim",
-    optional = true,
-    opts = function(_, opts)
-      local projects = { action = DotVim.fzf.projects, desc = "Projects", icon = "", key = "p" }
-      table.insert(opts.config.center, 3, projects)
-    end,
-  },
+  -- {
+  --   "nvimdev/dashboard-nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     local projects = { action = DotVim.fzf.projects, desc = "Projects", icon = "", key = "p" }
+  --     table.insert(opts.config.center, 3, projects)
+  --   end,
+  -- },
 
   {
     "folke/snacks.nvim",
     optional = true,
     opts = function(_, opts)
       table.insert(opts.dashboard.preset.keys, 3, {
-        action = DotVim.fzf.projects,
+        action = ":lua Snacks.picker.projects()",
         desc = "Projects",
         icon = " ",
         key = "p",
